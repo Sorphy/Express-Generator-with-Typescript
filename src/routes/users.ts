0750932845c196223bbfd560a6d2c0c3c4dd365f
Sofiyyah <sofiyyah.abidoye@decagon.dev>
2023-03-21 12:59:59 +0100

    express generator TS

---
+++ src/routes/users.ts
@@ -0,0 +1,10 @@
+import express, {Application, Request, Response, NextFunction} from 'express';
+
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+export default router;
